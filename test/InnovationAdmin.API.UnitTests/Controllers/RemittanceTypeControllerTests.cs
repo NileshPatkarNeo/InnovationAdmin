732f22f94aa11f4f5fd6763c9ba97521eab1fc08
Nupur <nupurmahajan6@gmail.com>
2024-06-11 10:25:21 +0530

    Testing Done in Remittance Type

--- test/InnovationAdmin.API.UnitTests/Controllers/RemittanceTypeControllerTests.cs
+++ test/InnovationAdmin.API.UnitTests/Controllers/RemittanceTypeControllerTests.cs
@@ -1,5 +1,8 @@
 ﻿using InnovationAdmin.Api.Controllers;
 using InnovationAdmin.Application.Features.RemittanceType.Commands.CreateRemittanceType;
+using InnovationAdmin.Application.Features.RemittanceType.Commands.DeleteRemittanceType;
+using InnovationAdmin.Application.Features.RemittanceType.Commands.UpdateRemittanceType;
+using InnovationAdmin.Application.Features.RemittanceType.Queries.GetRemittanceTypeQuery;
 using InnovationAdmin.Application.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -70,5 +73,143 @@ namespace InnovationAdmin.API.UnitTests.Controllers
             var okResult = Assert.IsType<OkObjectResult>(result);
             Assert.Equal(response, okResult.Value);
         }
+
+        [Fact]
+        public async Task DeleteRemittanceTypeReturnsOk()
+        {
+            
+            var id = Guid.NewGuid();
+            _mediatorMock.Setup(m => m.Send(It.IsAny<DeleteRemittanceTypeCommand>(), default))
+                         .ReturnsAsync(new Response<bool> { Data = true, Succeeded = true });
+
+            var result = await _controller.Delete(id);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<Response<bool>>(okResult.Value);
+            Assert.True(response.Data);
+        }
+
+        [Fact]
+        public async Task DeleteRemittanceTypeReturnsInternalServerError()
+        {
+            var id = Guid.NewGuid();
+            _mediatorMock.Setup(m => m.Send(It.IsAny<DeleteRemittanceTypeCommand>(), default))
+                         .ReturnsAsync(new Response<bool> { Data = false, Succeeded = false });
+
+            var result = await _controller.Delete(id);
+
+            var statusCodeResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task Delete_RemittanceType_NotFound_ReturnsNotFound()
+        {
+
+            var id = Guid.NewGuid();
+            _mediatorMock.Setup(m => m.Send(It.IsAny<DeleteRemittanceTypeCommand>(), default))
+                         .ReturnsAsync(new Response<bool> { Data = false, Succeeded = true });
+
+            var result = await _controller.Delete(id);
+
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            var response = Assert.IsType<Response<bool>>(notFoundResult.Value);
+            Assert.False(response.Data);
+        }
+
+        [Fact]
+        public async Task Update_RemittanceType_IdMismatch_ReturnsBadRequest()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var command = new UpdateRemittanceTypeCommand { Id = Guid.NewGuid(), Name = "New Name" };
+
+            // Act
+            var result = await _controller.Update(id, command);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("ID mismatch", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task Update_RemittanceType_Success_ReturnsOk()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var command = new UpdateRemittanceTypeCommand { Id = id, Name = "Updated Name" };
+            var response = new Response<UpdateRemittanceTypeDto> { Succeeded = true, Data = new UpdateRemittanceTypeDto { Id = id, Name = "Updated Name" } };
+            _mediatorMock.Setup(m => m.Send(It.IsAny<UpdateRemittanceTypeCommand>(), default))
+                         .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.Update(id, command);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var responseObject = Assert.IsType<Response<UpdateRemittanceTypeDto>>(okResult.Value);
+            Assert.True(responseObject.Succeeded);
+            Assert.Equal("Updated Name", responseObject.Data.Name);
+        }
+
+        [Fact]
+        public async Task UpdateRemittanceTypeReturnsBadRequest()
+        {
+            var id = Guid.NewGuid();
+            var command = new UpdateRemittanceTypeCommand { Id = id, Name = "Updated Name" };
+            var response = new Response<UpdateRemittanceTypeDto> { Succeeded = false, Message = "Update failed" };
+            _mediatorMock.Setup(m => m.Send(It.IsAny<UpdateRemittanceTypeCommand>(), default))
+                         .ReturnsAsync(response);
+
+            var result = await _controller.Update(id, command);
+
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var responseObject = Assert.IsType<Response<UpdateRemittanceTypeDto>>(badRequestResult.Value);
+            Assert.False(responseObject.Succeeded);
+            Assert.Equal("Update failed", responseObject.Message);
+        }
+
+        [Fact]
+        public async Task GetRemittanceTypeByIdInvalidIdFormatReturnsBadRequest()
+        {
+            var invalidId = "invalid-guid";
+
+            var result = await _controller.GetRemittanceTypeById(invalidId);
+
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Invalid ID format.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task GetRemittanceTypeByIdReturnsNotFound()
+        {
+
+            var id = Guid.NewGuid().ToString();
+            _mediatorMock.Setup(m => m.Send(It.IsAny<GetRemittanceTypeByIdQuery>(), default))
+                         .ReturnsAsync((Response<RemittanceTypeDto>)null);
+
+            var result = await _controller.GetRemittanceTypeById(id);
+
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal("Remittance Type not found.", notFoundResult.Value);
+        }
+
+        [Fact]
+        public async Task GetRemittanceTypeByIdReturnsOk()
+        {
+            var id = Guid.NewGuid();
+            var remittanceTypeDto = new RemittanceTypeDto { Id = id, Name = "Sample Name" };
+            var response = new Response<RemittanceTypeDto> { Succeeded = true, Data = remittanceTypeDto };
+            _mediatorMock.Setup(m => m.Send(It.IsAny<GetRemittanceTypeByIdQuery>(), default))
+                         .ReturnsAsync(response);
+
+            var result = await _controller.GetRemittanceTypeById(id.ToString());
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var responseObject = Assert.IsType<Response<RemittanceTypeDto>>(okResult.Value);
+            Assert.True(responseObject.Succeeded);
+            Assert.Equal("Sample Name", responseObject.Data.Name);
+        }
+
     }
 }
