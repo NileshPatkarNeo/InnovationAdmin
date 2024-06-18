b20cc9a2967ef5f45964f7a118674e4e0419ed1f
Mayurpawar5121 <mp854719@gmail.com>
2024-06-18 19:05:41 +0530

    Contract Table WebAPI Done

---
+++ src/Core/InnovationAdmin.Application/Features/ContractTerms/Queries/GetContractTerm/GetContractTermDetailQuery.cs
@@ -0,0 +1,11 @@
+﻿using InnovationAdmin.Application.Responses;
+using MediatR;
+using System;
+
+namespace InnovationAdmin.Application.Features.ContractTerms.Queries.GetContractTerm
+{
+    public class GetContractTermDetailQuery : IRequest<Response<ContractTermVM>>
+    {
+        public Guid ID { get; set; }
+    }
+}
