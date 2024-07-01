8fff11598d56508b64bd31fc2a785062ae4ee88f
Nupur <nupurmahajan6@gmail.com>
2024-07-01 16:28:13 +0530

    commit changes

--- src/UI/Innovation_Admin.UI/Models/CategoryType/CategoryTypeDto.cs
+++ src/UI/Innovation_Admin.UI/Models/CategoryType/CategoryTypeDto.cs
@@ -8,28 +8,28 @@ namespace Innovation_Admin.UI.Models.CategoryType
         [JsonProperty("ID")]
         public Guid ID { get; set; }
 
-        [Required(ErrorMessage = "Name is required.")]
-        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
-        [MinLength(2, ErrorMessage = "Name should have at least 2 characters.")]
-        [RegularExpression(@"^[a-zA-Z0-9\s]*$", ErrorMessage = "Name can only contain alphanumeric characters and spaces.")]
+        [Required(ErrorMessage = "Document Name is required.")]
+        [MaxLength(50, ErrorMessage = "Document Name cannot exceed 50 characters.")]
+        [MinLength(2, ErrorMessage = "Document Name should have at least 2 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Document Name can only contain alphanumeric characters and spaces.")]
         public string DocumentName { get; set; }
 
-        [Required(ErrorMessage = "Name is required.")]
-        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
-        [MinLength(2, ErrorMessage = "Name should have at least 2 characters.")]
-        [RegularExpression(@"^[a-zA-Z0-9\s]*$", ErrorMessage = "Name can only contain alphanumeric characters and spaces.")]
+        [Required(ErrorMessage = "Group Name is required.")]
+        [MaxLength(50, ErrorMessage = "Group Name cannot exceed 50 characters.")]
+        [MinLength(2, ErrorMessage = "Group Name should have at least 2 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Group Name can only contain alphanumeric characters and spaces.")]
         public string GroupName { get; set; }
 
-        [Required(ErrorMessage = "Name is required.")]
-        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
-        [MinLength(2, ErrorMessage = "Name should have at least 2 characters.")]
-        [RegularExpression(@"^[a-zA-Z0-9\s]*$", ErrorMessage = "Name can only contain alphanumeric characters and spaces.")]
+        [Required(ErrorMessage = "Claim Name is required.")]
+        [MaxLength(50, ErrorMessage = "Claim Name cannot exceed 50 characters.")]
+        [MinLength(2, ErrorMessage = "Claim Name should have at least 2 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Claim Name can only contain alphanumeric characters and spaces.")]
         public string ClaimName { get; set; }
 
-        [Required(ErrorMessage = "Name is required.")]
-        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
-        [MinLength(2, ErrorMessage = "Name should have at least 2 characters.")]
-        [RegularExpression(@"^[a-zA-Z0-9\s]*$", ErrorMessage = "Name can only contain alphanumeric characters and spaces.")]
+        [Required(ErrorMessage = "Adjustment Name is required.")]
+        [MaxLength(50, ErrorMessage = "Adjustment Name cannot exceed 50 characters.")]
+        [MinLength(2, ErrorMessage = "Adjustment Name should have at least 2 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Adjustment Name can only contain alphanumeric characters and spaces.")]
         public string AdjustmentName { get; set; }
 
         [StringLength(1000)]
